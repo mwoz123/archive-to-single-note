3bb7d8170eafff1bd1f7c2a424e2c8c1b1f3d21c
mwoz123 <4487025+mwoz123@users.noreply.github.com>
2023-11-13 18:19:32 +0100

    wip: multiple archives files

--- main.ts
+++ main.ts
@@ -1,14 +1,14 @@
-import { App, Editor, MarkdownView, Plugin, PluginSettingTab, Setting, TFile } from 'obsidian';
+import { App, ButtonComponent, Editor, MarkdownView, Plugin, PluginSettingTab, Setting, TFile } from 'obsidian';
 
 
 interface PluginSettings {
 	archiveFile: string;
-	archiveHobbyFile: string;
+	additionalArchives: [string];
 }
 
 const DEFAULT_SETTINGS: PluginSettings = {
 	archiveFile: 'archive.md',
-	archiveHobbyFile: 'hobby/archive.md'
+	additionalArchives: ['hobby/archive.md']
 }
 
 export default class ArchiveToSingleFilePlugin extends Plugin {
@@ -27,12 +27,12 @@ export default class ArchiveToSingleFilePlugin extends Plugin {
 				archiveFile(editor, this.app, this.settings.archiveFile)
 		});
 
-		this.addCommand({
-			id: 'archive-to-additional-file',
-			name: 'Archive to additional (e.g. hobby) file',
-			editorCallback: (editor: Editor, view: MarkdownView) =>
-				archiveFile(editor, this.app, this.settings.archiveHobbyFile)
-		});
+		// this.addCommand({
+		// 	id: 'archive-to-additional-file',
+		// 	name: 'Archive to additional (e.g. hobby) file',
+		// 	editorCallback: (editor: Editor, view: MarkdownView) =>
+		// 		// archiveFile(editor, this.app, this.settings.archiveHobbyFile)
+		// });
 
 		this.addSettingTab(new ArchiveToSingleFilePluginSettingTab(this.app, this));
 	}
@@ -90,16 +90,36 @@ class ArchiveToSingleFilePluginSettingTab extends PluginSettingTab {
 					this.plugin.settings.archiveFile = value;
 					await this.plugin.saveSettings();
 				}));
+		new Setting(containerEl).addButton((cb: ButtonComponent) => {
+			cb.setButtonText("Add");
+			cb.onClick(() => {
+				this.plugin.settings.additionalArchives.push("archive-" + this.plugin.settings.additionalArchives.length + ".md")
+				containerEl.empty();
+				this.display();
+			})
+		});
 
-		new Setting(containerEl)
-			.setName('Additional (e.g. hobby, work) archive')
-			.setDesc('with folder prefix (if required)')
-			.addText(text => text
-				.setPlaceholder('hobby/archive.md')
-				.setValue(this.plugin.settings.archiveHobbyFile)
-				.onChange(async (value) => {
-					this.plugin.settings.archiveHobbyFile = value;
-					await this.plugin.saveSettings();
-				}));
+		this.plugin.settings.additionalArchives.map((archiveName, index, array) => {
+			console.log(array)
+			new Setting(containerEl)
+				.setName('Archive ' + archiveName)
+				.setDesc('with folder prefix (if required)')
+				.addText(text => text
+					.setValue(archiveName)
+					.onChange(async (value) => {
+						this.plugin.settings.additionalArchives[index] = value;
+						await this.plugin.saveSettings();
+					}))
+			new Setting(containerEl).addButton((cb: ButtonComponent) => {
+				cb.setButtonText("Remove");
+				cb.onClick(() => {
+					array.splice(index, 1)
+					containerEl.empty();
+					this.display();
+				})
+			}
+			);
+
+		})
 	}
 }
